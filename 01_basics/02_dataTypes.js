3c6c87d2fc7358a04dbf983aca9e812f64509b0f
Shivam Singh <69629252+Shivam-Singh62@users.noreply.github.com>
2025-06-06 15:10:12 +0000

    02_dataTypes information

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+const name="shivam";
+
+let name2="shivam singh";
+let age=23;
+let isLoogedIn=true;
+let salary;
+let state=null;
+
+//number=>2to the power 55
+//bigInt
+//string= "zdsdvcds";
+//boolean =>true/false;
+//null = syandalone value;
+//undefined=
+//symbol = uniques
+
+
+//object
+console.log(typeof "shivam")
+
+//null is object in typeof
+//undefined is undefined
